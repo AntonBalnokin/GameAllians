dff8c002af8aaab3706b83d126331f7c86165fe1
Balnokin Anton <abalnokin@mail.ru>
2022-08-30 15:19:15 +0300

    2nd comm

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,6 @@
-import sys, pygame
+import sys
+import pygame
+from settings import Settings
 
 
 class AlienInvasion:
@@ -6,8 +8,12 @@ class AlienInvasion:
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы"""
         pygame.init()
-        self.screen = pygame.display.set_mode((2200, 1300))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                              self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
+        # назначение цвета фона
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Запуск основного цикла игры"""
@@ -16,9 +22,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.settings.bg_color)
             # отображение последнего прорисованного экрана
             pygame.display.flip()
-
 
 
 if __name__ == '__main__':
